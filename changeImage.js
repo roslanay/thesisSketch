982ccb330bec4bef6caaf80ebd5088cd3d7e797b
Roslana Yotova <roslanayotova@gmail.com>
2019-09-20 00:38:42 +0300

    Add files via upload

---
+++ changeImage.js
@@ -0,0 +1,11 @@
+$(function(){
+  $("#change").on({
+   mouseenter: function(){
+    $(this).attr('src','http://www.clker.com/cliparts/d/3/R/P/q/e/hot-pink-home-icon-md.png');
+  },
+  mouseleave: function(){
+    $(this).attr('src','http://openclipart.org/image/800px/svg_to_png/17103/claudita_home_icon.png');
+  }
+  });
+  
+});
